d3e101eabb8cfc37aee44abe8d96e0a33a046204
Matthew Berryman <mattjb@me.com>
2017-03-29 13:32:49 +1100

    Tidy up of app.js: * Remove use of express—according to node pg docs it should keep running   anyway and we don't otherwise need express. Can always add back in. * Add missing semicolons;

--- app.js
+++ app.js
@@ -4,72 +4,63 @@ require('dotenv').config({silent:true});
 const AWS = require('aws-sdk');
 AWS.config.update({region:process.env.AWS_REGION});
 
-var express = require('express')
-var app = express()
-var sns = new AWS.SNS()
+var sns = new AWS.SNS();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+pg.connect(process.env.PG_CON, function(err, client, done) {
+  console.log("Database connection successful");
+  if (err){
+    console.log("database err: " + err);
+    done();
+  }
+  // Return the listen notification
+  client.on('notification', function(msg) {
+    try {
+      console.log('Msg: ' + msg);
+      console.log('Payload: ' + msg.payload);
+      var notification = JSON.parse(msg.payload);
+      console.log('Parse successful');
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-  // Connect to db
-  pg.connect(process.env.PG_CON, function(err, client, done){
-     console.log("Database connection successful");
-     if (err){
-       console.log("database err: " + err);
-       done();
-     }
-     // Return the listen notification
-     client.on('notification', function(msg) {
-       try {
-         console.log('Msg: ' + msg);
-         console.log('Payload: ' + msg.payload);
-         var notification = JSON.parse(msg.payload);
-         console.log('Parse successful');
+      var topicName = "";
+      if (notification.cards.network === 'facebook'){
+        console.log('Received card submission via Facebook');
+        topicName = "Facebook";
+      } else if (notification.cards.network === 'telegram') {
+        console.log('Received card submission via Telegram');
+        topicName = "Telegram";
+      } else if (notification.cards.network === 'twitter') {
+        console.log('Received card submission via Twitter');
+        topicName = "Twitter";
+      }
 
-         var topicName = "";
-         if (notification.cards.network === 'facebook'){
-           console.log('Received card submission via Facebook');
-           topicName = "Facebook";
-         } else if (notification.cards.network === 'telegram') {
-           console.log('Received card submission via Telegram');
-           topicName = "Telegram";
-         } else if (notification.cards.network === 'twitter') {
-           console.log('Received card submission via Twitter');
-           topicName = "Twitter";
-         }
-
-         //Construct JSON with relevant details for a confirmation response to be published to SNS topic
-         var jsonMessage = {
-           "language" : notification.cards.language,
-           "username" : notification.cards.username,
-           "implementation_area": notification.cards.report_impl_area,
-           "report_id": notification.cards.report_id
-         };
-         if(topicName !== "")
-         {
-           //Construct message payload
-           var params = {
-             Message: JSON.stringify(jsonMessage),
-             TopicArn: "arn:aws:sns:" + process.env.AWS_REGION + ":" + process.env.ACCOUNTID + ":" + topicName
-           };
-           console.log("Publishing to " + topicName + " SNS topic");
-           sns.publish(params, function(err, data) {
-             if (err) {
-               console.log("Error on publishing message to topic" + topicName);
-               console.log(err);
-             } else {
-               console.log("Message published to " + topicName + " SNS topic successfully")
-               console.log(data);
-             }
-           });
-         }
-       } catch (e){
-         console.log('Error processing listen notification from database\n'+e);
-       }
-     });
-     // Initiate the listen query
-     client.query("LISTEN watchers");
+      //Construct JSON with relevant details for a confirmation response to be published to SNS topic
+      var jsonMessage = {
+        "language" : notification.cards.language,
+        "username" : notification.cards.username,
+        "implementation_area": notification.cards.report_impl_area,
+        "report_id": notification.cards.report_id
+      };
+      if(topicName !== "")
+      {
+        //Construct message payload
+        var params = {
+          Message: JSON.stringify(jsonMessage),
+          TopicArn: "arn:aws:sns:" + process.env.AWS_REGION + ":" + process.env.ACCOUNTID + ":" + topicName
+        };
+        console.log("Publishing to " + topicName + " SNS topic");
+        sns.publish(params, function(err, data) {
+          if (err) {
+            console.log("Error on publishing message to topic" + topicName);
+            console.log(err);
+          } else {
+            console.log("Message published to " + topicName + " SNS topic successfully");
+            console.log(data);
+          }
+        });
+      }
+    } catch (e){
+      console.log('Error processing listen notification from database\n'+e);
+    }
   });
-})
+  // Initiate the listen query
+  client.query("LISTEN watchers");
+});
